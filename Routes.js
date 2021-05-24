c8aff3b6215145b5ed57bd1576dc514b13312ee7
Rafique <rasgalazy5@gmail.com>
2021-05-24 12:56:44 +0000

    Heroku update 3

--- Routes.js
+++ Routes.js
@@ -4,12 +4,11 @@ const checkUser = require("./Middleware");
 const Router = express();
 const uuid = require("uuid").v4;
 const { genPassword, validPassword } = require("./lib/Password");
-const passport = require('passport')  //Imports Passport
-const jwt = require('jsonwebtoken')
+const passport = require("passport"); //Imports Passport
+const jwt = require("jsonwebtoken");
 
-
-const expirationtimeInMs = 600000
-const secret = "wow123"
+const expirationtimeInMs = 600000;
+const secret = "wow123";
 
 //Login Route
 Router.post("/login", async function (request, response) {
@@ -18,30 +17,26 @@ Router.post("/login", async function (request, response) {
   let responseData = await Usermodel.findOne({ email });
 
   if (responseData) {
-   
+    const isValid = validPassword(
+      password,
+      responseData.salt,
+      responseData.hash
+    );
 
-    const isValid = validPassword(password,responseData.salt,responseData.hash)
-   
     if (isValid) {
-
       const payload = {
         email: responseData.email,
-        expiration: Date.now() + parseInt(expirationtimeInMs)
-    }
+        expiration: Date.now() + parseInt(expirationtimeInMs),
+      };
 
-    const token = jwt.sign(JSON.stringify(payload), secret)
-    response
-    .cookie('jwt',
-        token, {
-            httpOnly: true,
-            secure: false //--> SET TO TRUE ON PRODUCTION
-        }
-    )
-    // .status(200)
-    // .json({
-    //     message: 'You have logged in :D'
-    // })
-.status(200).send({ success: true, message: "Successful" });
+      const token = jwt.sign(JSON.stringify(payload), secret);
+      response
+        .cookie("jwt", token, {
+          httpOnly: true,
+          secure: false, //--> SET TO TRUE ON PRODUCTION
+        })
+       .status(200)
+        .send({ success: true, message: "Successful" });
     } else {
       response
         .status(300)
@@ -84,18 +79,19 @@ Router.post("/signup", async function (request, response) {
 
 // console.log('inside the homepage callback function')
 
-Router.post('loging', (request, response) => {
-  response.status(600).send('Helooo')
-})
+Router.post("loging", (request, response) => {
+  response.status(600).send("Helooo");
+});
 
 //Protected Route
 
-Router.get('/protected', 
-    passport.authenticate('jwt', { session: false }),
-    (req, res) => {
-        res.status(200).json({
-            message: 'welcome to the protected route!'
-        })
-    }
-)
+Router.get(
+  "/protected",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    res.status(200).json({
+      message: "welcome to the protected route!",
+    });
+  }
+);
 module.exports = Router;
